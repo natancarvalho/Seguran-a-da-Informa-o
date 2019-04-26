465383baa37c8c0244bd41b5196456efd2cc94d1
natancarvalho <natancarvalho@poli.ufrj.br>
2019-04-25 21:56:27 -0300

    Update portscanner.py

--- Trabalho 2 - Anatomia de um ataque (PortScan)/portscanner.py
+++ Trabalho 2 - Anatomia de um ataque (PortScan)/portscanner.py
@@ -1,1 +1,42 @@
+#!/usr/bin/env python
+import socket
+import subprocess
+import sys
 
+# Ask for input
+#print("Enter IP:")
+remoteServerIP = input("Enter IP:" )
+  
+
+# Print a nice banner with information on which host we are about to scan
+print("-" * 60)
+print("Please wait, scanning remote host", remoteServerIP)
+print("-" * 60)
+
+# We also put in some error handling for catching errors
+
+try:
+    for port in range(1,1025):  
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.settimeout(2)
+        try:
+            s.connect((remoteServerIP, port))
+            s.send(b'Respond True')
+            result = s.recv(1024)
+            if result:
+                print("Port {}: Result: {}".format(port, result))
+            else:
+                print("Port {}: No response.".format(port))
+            s.close()   
+        except socket.error:
+            print( "Couldn't connect to port {}".format(port))
+            pass
+
+except KeyboardInterrupt:
+    print("You pressed Ctrl+C")
+    sys.exit()
+
+except socket.gaierror:
+    print ('Hostname could not be resolved. Exiting')
+sys.exit()
